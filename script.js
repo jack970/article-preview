3d69623f73dd085b4e7b9dc558c1c9998234e0c1
Jack970 <italo@workinformatica.com.br>
2022-12-14 14:47:35 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const showSocials = () => {
+  const socials = document.getElementById("socials");
+
+  socials.style.opacity == "1"
+    ? (socials.style.opacity = "0")
+    : (socials.style.opacity = "1");
+};
